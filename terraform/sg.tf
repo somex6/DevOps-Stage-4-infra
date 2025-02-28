945fe9701beff2896c82ebe5e0d590c29d954648
somex6 <iwekasomex61@gmail.com>
2025-02-28 15:10:25 +0100

    test

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -9,3 +9,16 @@ module "security-group" {
 
   tags = var.tags
 }
+
+
+module "security-group-tes" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+
+  name          = "tset-sg"
+  description   = var.description
+  vpc_id        = module.vpc.vpc_id
+  ingress_rules = var.ingress_rules
+
+  tags = var.tags
+}
