c07a4fa23eaacfc439320f7595ca8b848b6e2585
somex6 <iwekasomex61@gmail.com>
2025-02-28 15:39:35 +0100

    fix duplicate

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -11,9 +11,9 @@
 # }
 
 
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
+resource "aws_security_group" "ingress" {
+  name        = "allow tcp"
+  description = "Allow inbound"
   vpc_id      = module.vpc.vpc_id
   ingress {
     description = "TLS from VPC"
